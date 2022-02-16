4eaa12b1000813f2b6297eca6269ea517b1f3dc2
lincolnokaalo <lincolnokaalo@gmail.com>
2022-02-16 15:55:28 +0300

    Initialized main.js and Module.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import productdb from "./Module.js";
+
+let db = productdb("Productdb", {
+    products:`++id, name, seller, price`
+});
+
+// input tags
+const userid = document.getElementById("userid");
+const proname = document.getElementById("proname");
+const seller = document.getElementById("seller");
+const price = document.getElementById("price");
+
+// buttons
+const btncreate = document.getElementById("btn-create");
+const btnread = document.getElementById("btn-read");
+const btnupdate = document.getElementById("btn-update");
+const btndelete = document.getElementById("btn-delete");
+
+// insert values using the create button
+btncreate.onclick
